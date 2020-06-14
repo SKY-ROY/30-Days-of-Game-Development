8676d2fa6f34cc61a1548e51835b5bf00f752672
SKY <akash.rm.roy@gmail.com>
2020-06-14 18:55:16 +0530

    AmazingPlatformer initialized.
    Signed-off-by: SKY <akash.rm.roy@gmail.com>

---
+++ AmazingPlatformer/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+
+    private void PlayerWalk()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if(h>0)
+        {
+            myBody.velocity = new Vector2(speed, myBody.velocity.y);
+        }
+        else if(h<0)
+        {
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+        }
+    }
+}
