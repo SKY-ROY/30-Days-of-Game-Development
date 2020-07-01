9890cf93118b75dd82dd450bd3aa9e5b243a683c
SKY <akash.rm.roy@gmail.com>
2020-07-01 23:40:20 +0530

    Added frog to the AmazingPlatformer projecct.
    Signed-off-by: SKY <akash.rm.roy@gmail.com>

---
+++ AmazingPlatformer/Assets/Scripts/EnemyScripts/FrogScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FrogScript : MonoBehaviour
+{
+    private Animator anim;
+
+    private bool animation_Started;
+    private bool animation_Finished;
+
+    private int jumpTimes;
+    private bool jumpLeft = true;
+
+    private string coroutine_Name = "FrogJump";
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(coroutine_Name);
+    }
+     
+    private void LateUpdate()
+    {
+        //Debug.Log("LateUpdate");
+        if(animation_Finished && animation_Started)
+        {
+            //Debug.Log("Position updated");
+            animation_Started = false;
+
+            transform.parent.position = transform.position;
+            transform.localPosition = Vector3.zero;
+        }
+    }
+    
+    IEnumerator FrogJump()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 4f));
+
+        animation_Started = true;
+        animation_Finished = false;
+
+        jumpTimes++;
+
+        if (jumpLeft)
+        {
+            anim.Play("FrogJumpLeft");
+        }
+        else
+        {
+            anim.Play("FrogJumpRight");
+        }
+
+        StartCoroutine(coroutine_Name);
+    }
+
+    void AnimationFinished()
+    {
+        //Debug.Log("Animation Finished");
+        animation_Finished = true;
+
+        if (jumpLeft)
+        {
+            anim.Play("FrogIdleLeft");
+        }
+        else
+        {
+            anim.Play("FrogIdleRight");
+        }
+
+        if (jumpTimes == 3)
+        {
+            jumpTimes = 0;
+
+            Vector3 tempScale = transform.localScale;
+            tempScale.x *= -1f;
+            transform.localScale = tempScale;
+
+            jumpLeft = !jumpLeft;
+        }
+    }
+}
